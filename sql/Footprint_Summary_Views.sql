24d79517b789458261e91e2720cc204228f0d8e0
Tom Buckley <buckley.tom@gmail.com>
2017-01-17 10:08:40 -0800

    update source table names for summary

--- sql/Footprint_Summary_Views.sql
+++ sql/Footprint_Summary_Views.sql
@@ -1,63 +1,63 @@
 --overall summary table
-CREATE VIEW UrbanSim.parcel_footprint_summary_by_run as
+CREATE VIEW Analysis.parcel_footprint_summary_by_run as
 SELECT 'run10' as tablename, 
 	count(*) AS parcel_count, 
-	ROUND(SUM(households),0) as sum_households, 
-	ROUND(SUM(jobs),0) as sum_jobs, 
 	ROUND(SUM(Acres),0) as sum_acres,
 	ROUND(AVG(Acres),2) as avg_acres,
 	ROUND(SUM(total_job_spaces),0) as sum_total_job_spaces,
+	ROUND(AVG(People_Per_Acre),2) as avg_people_per_acre,
+	ROUND(AVG(Jobs_Per_Acre),2) as avg_jobs_per_acre,
 	ROUND(SUM(total_residential_units),0) as sum_total_residential_units,
 	ROUND(AVG(people_per_acre),2) as avg_ppl_per_acre
-FROM [DEIR2017].[UrbanSim].run10_parcel_output_spatial
+FROM [DEIR2017].[Analysis].run10_parcel_output_eir_footprint_spatial
 UNION
 SELECT 'run11' as tablename, 
 	count(*) AS parcel_count, 
-	ROUND(SUM(households),0) as sum_households, 
-	ROUND(SUM(jobs),0) as sum_jobs, 
 	ROUND(SUM(Acres),0) as sum_acres,
 	ROUND(AVG(Acres),2) as avg_acres,
 	ROUND(SUM(total_job_spaces),0) as sum_total_job_spaces,
+	ROUND(AVG(People_Per_Acre),2) as avg_people_per_acre,
+	ROUND(AVG(Jobs_Per_Acre),2) as avg_jobs_per_acre,
 	ROUND(SUM(total_residential_units),0) as sum_total_residential_units,
 	ROUND(AVG(people_per_acre),2) as avg_ppl_per_acre
-FROM [DEIR2017].[UrbanSim].run11_parcel_output_spatial
+FROM [DEIR2017].[Analysis].run11_parcel_output_eir_footprint_spatial
 UNION
 SELECT 'run12' as tablename, 
 	count(*) AS parcel_count, 
-	ROUND(SUM(households),0) as sum_households, 
-	ROUND(SUM(jobs),0) as sum_jobs, 
 	ROUND(SUM(Acres),0) as sum_acres,
 	ROUND(AVG(Acres),2) as avg_acres,
 	ROUND(SUM(total_job_spaces),0) as sum_total_job_spaces,
+	ROUND(AVG(People_Per_Acre),2) as avg_people_per_acre,
+	ROUND(AVG(Jobs_Per_Acre),2) as avg_jobs_per_acre,
 	ROUND(SUM(total_residential_units),0) as sum_total_residential_units,
 	ROUND(AVG(people_per_acre),2) as avg_ppl_per_acre
-FROM [DEIR2017].[UrbanSim].run12_parcel_output_spatial
+FROM [DEIR2017].[Analysis].run12_parcel_output_eir_footprint_spatial
 UNION
 SELECT 'run17' as tablename, 
 	count(*) AS parcel_count, 
-	ROUND(SUM(households),0) as sum_households, 
-	ROUND(SUM(jobs),0) as sum_jobs, 
 	ROUND(SUM(Acres),0) as sum_acres,
 	ROUND(AVG(Acres),2) as avg_acres,
 	ROUND(SUM(total_job_spaces),0) as sum_total_job_spaces,
+	ROUND(AVG(People_Per_Acre),2) as avg_people_per_acre,
+	ROUND(AVG(Jobs_Per_Acre),2) as avg_jobs_per_acre,
 	ROUND(SUM(total_residential_units),0) as sum_total_residential_units,
 	ROUND(AVG(people_per_acre),2) as avg_ppl_per_acre
-FROM [DEIR2017].[UrbanSim].run17_parcel_output_spatial
+FROM [DEIR2017].[Analysis].run17_parcel_output_eir_footprint_spatial
 UNION
 SELECT 'run7224' as tablename, 
 	count(*) AS parcel_count, 
-	ROUND(SUM(households),0) as sum_households, 
-	ROUND(SUM(jobs),0) as sum_jobs, 
 	ROUND(SUM(Acres),0) as sum_acres,
 	ROUND(AVG(Acres),2) as avg_acres,
 	ROUND(SUM(total_job_spaces),0) as sum_total_job_spaces,
+	ROUND(AVG(People_Per_Acre),2) as avg_people_per_acre,
+	ROUND(AVG(Jobs_Per_Acre),2) as avg_jobs_per_acre,
 	ROUND(SUM(total_residential_units),0) as sum_total_residential_units,
 	ROUND(AVG(people_per_acre),0) as avg_ppl_per_acre
-  FROM [DEIR2017].[UrbanSim].run7224_parcel_output_spatial
+  FROM [DEIR2017].[Analysis].run7224_parcel_output_eir_footprint_spatial
 
 
 --summary view of all scenarios on the parcels
-CREATE VIEW UrbanSim.parcel_summary_runs_10_11_12_17_7224 AS
+CREATE VIEW Analysis.parcel_summary_runs_10_11_12_17_7224 AS
 SELECT  run7224.county_id, 
 		run7224.parcel_id as parcel_id_7224,
 		run10.parcel_id as parcel_id_10,
@@ -104,14 +104,14 @@ SELECT  run7224.county_id,
 		run12.Jobs_Per_Acre as Jobs_Per_Acre_12,
 		run17.Jobs_Per_Acre as Jobs_Per_Acre_17,
 		run7224.Jobs_Per_Acre as Jobs_Per_Acre_7224
-FROM UrbanSim.run7224_parcel_output_spatial as run7224
-FULL OUTER JOIN UrbanSim.run12_parcel_output_spatial as run12 
+FROM Analysis.run7224_parcel_output_eir_footprint_spatial as run7224
+FULL OUTER JOIN Analysis.run12_parcel_output_eir_footprint_spatial as run12 
 ON run7224.parcel_id = run12.parcel_id
-FULL OUTER JOIN UrbanSim.run11_parcel_output_spatial as run11 
+FULL OUTER JOIN Analysis.run11_parcel_output_eir_footprint_spatial as run11 
 ON run12.parcel_id = run11.parcel_id
-FULL OUTER JOIN UrbanSim.run17_parcel_output_spatial as run17 
+FULL OUTER JOIN Analysis.run17_parcel_output_eir_footprint_spatial as run17 
 ON run11.parcel_id = run17.parcel_id
-FULL OUTER JOIN UrbanSim.run10_parcel_output_spatial as run10
+FULL OUTER JOIN Analysis.run10_parcel_output_eir_footprint_spatial as run10
 ON run17.parcel_id = run10.parcel_id
 WHERE run7224.parcel_id IS NOT NULL AND
 run10.parcel_id IS NOT NULL AND
